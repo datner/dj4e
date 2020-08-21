325af40472c09270d09718c57295c6cc2a74f213
Datner <yuval.datner.alt@gmail.com>
2020-08-22 01:03:57 +0300

    add a lot of cat stuff

--- cats/views.py
+++ cats/views.py
@@ -13,14 +13,14 @@ class CatList(LoginRequiredMixin, View) :
         bc = Breed.objects.all().count();
         cl = Cat.objects.all();
 
-        ctx = { 'make_count': bc, 'auto_list': cl };
-        return render(request, 'autos/auto_list.html', ctx)
+        ctx = { 'breed_count': bc, 'cat_list': cl };
+        return render(request, 'cats/cat_list.html', ctx)
 
 class BreedView(LoginRequiredMixin,View) :
     def get(self, request):
-        ml = Breed.objects.all();
-        ctx = { 'make_list': ml };
-        return render(request, 'autos/make_list.html', ctx)
+        bl = Breed.objects.all();
+        ctx = { 'breed_list': bl };
+        return render(request, 'cats/cat_list.html', ctx)
 
 # We use reverse_lazy() because we are in "constructor attribute" code
 # that is run before urls.py is completely loaded
@@ -28,32 +28,32 @@ class BreedView(LoginRequiredMixin,View) :
 class CatCreate(LoginRequiredMixin,CreateView):
     model = Cat
     fields = '__all__'
-    success_url = reverse_lazy('autos:all')
+    success_url = reverse_lazy('cats:all')
 
 class CatUpdate(LoginRequiredMixin, UpdateView):
     model = Cat
     fields = '__all__'
-    success_url = reverse_lazy('autos:all')
+    success_url = reverse_lazy('cats:all')
 
 class CatDelete(LoginRequiredMixin, DeleteView):
     model = Cat
     fields = '__all__'
-    success_url = reverse_lazy('autos:all')
+    success_url = reverse_lazy('cats:all')
 
 class BreedCreate(LoginRequiredMixin, CreateView):
   model = Breed
   fields = '__all__'
-  success_url = reverse_lazy('autos:all')
+  success_url = reverse_lazy('cats:all')
 
 class BreedUpdate(LoginRequiredMixin, UpdateView):
   model = Breed
   fields = '__all__'
-  success_url = reverse_lazy('autos:all')
+  success_url = reverse_lazy('cats:all')
 
 class BreedDelete(LoginRequiredMixin, DeleteView):
   model = Breed
   fields = '__all__'
-  success_url = reverse_lazy('autos:all')
+  success_url = reverse_lazy('cats:all')
 
 # We use reverse_lazy rather than reverse in the class attributes
 # because views.py is loaded by urls.py and in urls.py as_view() causes
